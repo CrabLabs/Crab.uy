01167af5a2e24b81df32a18e20e01801ec4e6ed2
Rodrigo Espinosa <espinosacurbelo@gmail.com>
2013-09-18 00:37:59 -0300

    main.js con smooth

--- js/main.js
+++ js/main.js
@@ -2,7 +2,21 @@
 	"use strict";
 
 	$(document).on("ready", function () {
+		$("a[href*=#]").click(function() {
+			if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+				var targetOffset, $target = $(this.hash);
+				$target = $target.length && $target || $("[name=" + this.hash.slice(1) + "]");
+				
+				if ($target.length) {
+					targetOffset = $target.offset().top;
+					$("html, body").animate({
+						scrollTop: targetOffset
+					}, 1000);
 
+					return false;
+				}
+			}
+		});
 	});
 
 	$(".posone").on("click", function (event) {
